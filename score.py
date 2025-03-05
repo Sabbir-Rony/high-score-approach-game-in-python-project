1ee174c979f5956ea609f21ce38df05a80d63c2b
Sabbir Ahmed Rony <160509348+Sabbir-Rony@users.noreply.github.com>
2025-03-05 14:28:45 +0600

    Add files via upload

---
+++ score.py
@@ -0,0 +1,25 @@
+import random
+
+def game():
+    print("You Are Playing The Game: ")
+
+    score = random.randint(1,62)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore !=""):
+            hiscore = int(hiscore)
+
+        else:
+            hiscore = 0
+
+    print(f"Your score: {score}")
+    if(score > hiscore):
+        with open("hiscore.txt","w") as f:
+            f.write(str(score))
+
+    return score
+game()
+
+
+
